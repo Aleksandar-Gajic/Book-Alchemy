2b6a33eb4d7e7208e245d7a733a8551d5a040908
Aleksandar-Gajic <aleksandargajic@ymail.com>
2026-03-12 10:49:11 +0100

    Made it look nicer

--- app.py
+++ app.py
@@ -143,4 +143,4 @@ def book_detail(book_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5005)
